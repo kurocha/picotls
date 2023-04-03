17c17aef525181a4c82101832b0692b1a0e814b0
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-04-03 21:33:34 +1200

    Initial configuration.

---
+++ teapot.rb
@@ -0,0 +1,34 @@
+#
+#  This file is part of the "Teapot" project, and is released under the MIT license.
+#
+
+teapot_version "3.0"
+
+define_target "picotls" do |target|
+	target.depends :platform
+	
+	target.depends "Build/Make"
+	target.depends "Build/CMake"
+	
+	target.provides "Library/picotls" do
+		source_files = target.package.path + "picotls"
+		cache_prefix = environment[:build_prefix] / environment.checksum + "picotls"
+		package_files = cache_prefix / "lib/libpicotls-core.a"
+		
+		cmake source: source_files, install_prefix: cache_prefix, arguments: [
+			"-DBUILD_SHARED_LIBS=OFF",
+		], package_files: package_files
+		
+		append linkflags package_files
+		append header_search_paths cache_prefix + "include"
+	end
+end
+
+define_configuration "picotls" do |configuration|
+	configuration[:source] = "https://github.com/kurocha/"
+	
+	configuration.require "platforms"
+	
+	configuration.require "build-make"
+	configuration.require "build-cmake"
+end
